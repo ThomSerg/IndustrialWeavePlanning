2482f258f2f671ea57553b12a86588b7f8d28cec
Thomas <thomas.s2000@hotmail.com>
2023-06-28 13:45:15 +0200

    2D intvar extension with instance level lower and upper bound

--- src/utils/cpm_extensions.py
+++ src/utils/cpm_extensions.py
@@ -5,3 +5,16 @@ def intvar_1D(lower, upper, repeat=1):
     if repeat == 1:
         return cpm_array([intvar(lower, upper)])
     return intvar(lower, upper, repeat)
+
+def intvar_2D(lowers, uppers):
+    vars = []
+    for lower_row, upper_row in zip(lowers, uppers):
+        temp_vars = []
+        for lower, upper in zip(lower_row, upper_row):
+            temp_vars.append(intvar(lower, upper))
+        vars.append(temp_vars)
+
+    if vars == []:
+            vars = [[]]
+            
+    return cpm_array(vars)
